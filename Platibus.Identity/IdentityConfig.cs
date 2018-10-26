14ae85ea25520563b2f7e9c72ead7c8b27295ab0
Ulrik Sandberg <ulrik.sandberg@champagnemoments.eu>
2018-10-26 17:54:39 +0200

    Initial check-in of module Platibus.Identity

---
+++ Platibus.Identity/IdentityConfig.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using IdentityServer4;
+namespace Platibus.Identity
+{
+    public static class IdentityConfig
+    {
+		public static IEnumerable<ApiResource> GetApiResources()
+		{
+			return new List<ApiResource>
+			{
+				new ApiResource("Platibus.Backend")
+			};
+		}
+
+		public static IEnumerable<Client> GetClients()
+		{
+			return new List<Client>
+			{
+				new Client
+				{
+					ClientId = "client",
+
+					AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+					ClientSecrets =
+					{
+						new Secret("secret".Sha256())
+					},
+
+					AllowedScopes = { "Platibus.Backend"}
+				},
+                new Client
+				{
+                    ClientId = "ro.client",
+					AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+					ClientSecrets =
+					{
+						new Secret("secret".Sha256())
+					},
+					AllowedScopes = {"Platibus.Backend"}
+				},
+                new Client
+				{
+					ClientId = "mvc",
+					ClientName = "MVC Client",
+					AllowedGrantTypes = GrantTypes.Implicit,
+
+                    
+                    //Where to redirect to after login
+					RedirectUris = {"https://localhost:5020/signin-oidc"},
+
+                    //Where to redirect to after logout
+					PostLogoutRedirectUris = {"https://localhost:5020/signout-callback-oidc"},
+					RequireConsent = false,
+					
+					AllowedScopes = new List<string>
+					{
+						IdentityServerConstants.StandardScopes.OpenId,
+						IdentityServerConstants.StandardScopes.Profile
+					}
+				}
+			};
+		}
+
+		public static List<TestUser> GetUsers()
+		{
+			return new List<TestUser>
+			{
+				new TestUser
+				{
+					SubjectId = "1",
+					Username = "Ulsan",
+					Password = "1234"
+				}
+			};
+		}
+
+		public static IEnumerable<IdentityResource> GetIdentityResources()
+		{
+			return new List<IdentityResource>
+			{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile()
+			};
+		}
+    }
+}
+
