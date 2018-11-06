b697e4496beeebc74da89cb17eb00386c163356f
Ulrik Sandberg <ulrik.sandberg@champagnemoments.eu>
2018-11-06 18:36:45 +0100

    Completing CreateUserRequestModel to mitigate unused field

--- Platibus.Identity/RequestModels/CreateUserRequestModel.cs
+++ Platibus.Identity/RequestModels/CreateUserRequestModel.cs
@@ -2,7 +2,6 @@ namespace Platibus.Identity.CreateUserModels
 {
     public class CreateUserRequestModel
     {
-        public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public int AuthLevel { get; set; }
